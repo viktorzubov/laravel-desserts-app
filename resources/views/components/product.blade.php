0087d2a94b6c601ca714e8527efa1c2f225a530e
viktor <13069159+viktorzubov@users.noreply.github.com>
2026-05-27 01:16:05 +0300

    Enhance product component and layout: update button styling, fix spelling in shopping cart, and improve product list header

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,14 +1,20 @@
 @props(['product'])
 
-<li class="bg-rose-100 aspect-square rounded-lg">
+<li class="aspect-square rounded-lg">
     <article>
-        <img src="{{ Vite::asset('resources/images/' . $product->image) }}" alt="{{ $product->name }}">
-        <form method="POST">
+        <img class="aspect-square object-cover rounded-xl" src="{{ Vite::asset('resources/images/' . $product->image) }}"
+            alt="{{ $product->name }}">
+        <form method="POST" class="flex justify-center -mt-5">
             @csrf
-            <button type="submit">Add to cart</button>
+            <button
+                class="bg-white border border-rose-500 hover:border-red hover:text-red rounded-full px-8 h-10 font-medium flex gap-2 items-center"
+                type="submit">
+                <x-icons.add-to-cart />
+                <span>Add to cart</span>
+            </button>
         </form>
-        <p>{{ $product->category }}</p>
-        <h2>{{ $product->name }}</h2>
-        <p>{{ $product->formattedPrice() }}</p>
+        <p class="mt-4 text-rose-500">{{ $product->category }}</p>
+        <h2 class="text-lg font-medium">{{ $product->name }}</h2>
+        <p class="font-medium text-red">{{ $product->formattedPrice() }}</p>
     </article>
 </li>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -19,15 +19,15 @@
 
 <body class="max-w-360 mx-auto px-4 sm:px-6 lg:px-8 grid md:grid-cols-[1fr_480px] bg-rose-50 py-16 gap-8">
     <main>
-        <h1 class="text-4xl font-bold">Desserts</h1>
-        <ul class="grid sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 mt-10">
+        <h1 class="text-5xl font-bold">Desserts</h1>
+        <ul class="grid sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 mt-10">
             @foreach ($products as $product)
                 <x-product :product="$product" />
             @endforeach
         </ul>
     </main>
     <aside class="bg-white p-6 h-80">
-        Shoping cart
+        Shopping cart
     </aside>
 </body>
 
